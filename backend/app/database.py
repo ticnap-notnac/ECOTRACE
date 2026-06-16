f3bfeabbbbe44e72c639219c4e691dd3be3d2107
ticnap-notnac <ronaldpacioles9@gmail.com>
2026-06-16 23:34:35 +0800

    fix: automatically format render database url for asyncpg

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+db_url = settings.DATABASE_URL
+if db_url.startswith("postgresql://"):
+    db_url = db_url.replace("postgresql://", "postgresql+asyncpg://", 1)
+
+engine = create_async_engine(db_url, echo=True)
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
